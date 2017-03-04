c8b6f0df2b7fb91f5ceefc57f77578b81949c2ef
Xiao Han <han.xiao@aalto.fi>
2017-03-04 22:44:18 +0000

    using heap

--- han.py
+++ han.py
@@ -1,29 +1,25 @@
 
 # coding: utf-8
 
-# In[1]:
 import numpy as np
 from collections import defaultdict, namedtuple
 from tqdm import tqdm
+from heap import build_heap
 
-
-# In[2]:
 
 def split2int(s):
     return list(map(int, s.split()))
 
 
-# In[3]:
 
 Req = namedtuple('Request', ['v', 'e', 'n'])
 
 
-# In[4]:
-
 # data = 'me_at_the_zoo.in'
 # data = 'trending_today.in'
-# data = 'videos_worth_spreading.in'
-data = 'kittens.in'
+data = 'videos_worth_spreading.in'
+# data = 'kittens.in'
+
 outpath = 'outputs/{}.out'.format(data.split('.')[0])
 reqs = {}
 latency = {}
@@ -55,35 +51,34 @@ for v, reqs in tqdm(v2reqs.items()):
     for c in range(C):
         vc2reqs[(v, c)] = set([r for r in reqs if r.e in c2ends[c]])
 
-I = np.zeros((C, V), dtype=np.float)
+
+cv_pairs = []
+imps = []
 
 for c in tqdm(range(C)):
     for v in range(V):
         if (v, c) in vc2reqs:
-            I[c, v] = sum(r.n * (latency[(r.e, CENTER)] - latency[(r.e, c)])
-                          for r in vc2reqs[(v, c)]) / v2size[v]
-        else:
-            I[c, v] = 0
+            cv_pairs.append((c, v))
+            imps.append(sum(r.n * (latency[(r.e, CENTER)] - latency[(r.e, c)])
+                            for r in vc2reqs[(v, c)]) / v2size[v])
 
+cv_heap = build_heap(imps, cv_pairs)
 
 @profile
-def main(vc2reqs, v2size, latency, I, debug=False):
+def main(vc2reqs, v2size, latency, cv_heap, debug=False):
 
     caps = {i: X for i in range(C)}
     c2v = defaultdict(set)
     req2c = {}  # which cache serves *the* request
     
     while True:
-        candidates_tuples = np.dstack(
-            np.unravel_index(
-                np.argsort(I.ravel()), (C, V)))[0][::-1]
-        nnz = np.count_nonzero(I)
-        if nnz % 10 == 0:
-            print(nnz)
+        if cv_heap.size % 100 == 0:
+            print(cv_heap.size)
             print('caps remaining: {}'.format(sum(caps.values()) / (X * C)))
         success = False
-        for cb, vb in candidates_tuples:
-            if I[cb, vb] > 0 and caps[cb] >= v2size[vb]:
+        while cv_heap.size > 0:
+            imp, (cb, vb) = cv_heap.pop_max()
+            if imp > 0 and caps[cb] >= v2size[vb]:
 
                 if debug:
                     print('putting video {} (size {}) into cache {}'.format(vb, v2size[vb], cb))
@@ -95,10 +90,7 @@ def main(vc2reqs, v2size, latency, I, debug=False):
 
                 c2v[cb].add(vb)
                 success = True
-                I[cb, vb] = 0
                 break
-            else:
-                I[cb, vb] = 0
 
         if not success:
             # not enough capacity, we are done
@@ -107,31 +99,33 @@ def main(vc2reqs, v2size, latency, I, debug=False):
         # the video vb being put into cache cb
         # will make some change to req2c
         for r in v2reqs[vb]:
-            if r.e in c2ends[cb]:  # cb can serve r.e
+            if r.e in c2ends[cb]:  # cb can serve r
                 if r in req2c:
                     if latency[(r.e, req2c[r])] > latency[(r.e, cb)]:
                         req2c[r] = cb
                 else:
                     req2c[r] = cb
+                # caches that can serve r should update the score
+                
                 
         # update the I matrix
         for c in range(C):
-            improvement = 0
-            if I[c, vb] > 0 and (vb, c) in vc2reqs:
+            if (c, vb) in cv_heap.e2i and (vb, c) in vc2reqs:
+                improvement = 0
                 for r in vc2reqs[(vb, c)]:
-                    if r in req2c and r.e in c2ends[cb]:  # r already served by cb
-                        # latency[(r.e, req2c[r])]
-                        # latency[(r.e, c)]
+                    # cb serve r and r is served by some c
+                    if r in req2c and r.e in c2ends[cb]:
                         if latency[(r.e, req2c[r])] > latency[(r.e, c)]:
                             improvement += r.n * (latency[(r.e, req2c[r])] - latency[(r.e, c)])
                     else:
                         improvement += r.n * (latency[(r.e, CENTER)] - latency[(r.e, c)])
-                I[(c, vb)] = improvement / v2size[vb]
+                cv_heap.update_key((c, vb), improvement / v2size[vb])
+
     if debug:
         print('remaining caps: {}'.format(caps))
     return c2v
 
-c2v = main(vc2reqs, v2size, latency, I, debug=False)
+c2v = main(vc2reqs, v2size, latency, cv_heap, debug=False)
 
 with open(outpath, 'w') as f:
     f.write('{}\n'.format(len(c2v)))
